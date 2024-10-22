15b224baacab5c7bc257ec4ea8a6ec7ef400506a
stephanezab <stephanezab@gmail.com>
2024-10-22 18:27:31 -0500

    Update displayMatches.jsx

--- ligo/app/displayMatches.jsx
+++ ligo/app/displayMatches.jsx
@@ -29,7 +29,7 @@ export default function displayMatches() {
             setUsersfound(list)
 
         } catch (error) {
-            console.log("Error get users ", error);
+            console.log("Error get users nearby", error);
         }
 
     }
